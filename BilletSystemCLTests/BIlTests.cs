01c22ee482b8d33bdcd776032e02bfe94e819666
Dshy <KortSkg@KORTSKG-PC>
2017-08-30 12:31:14 +0200

    Har tilføjet bil og mc samt testet dem

---
+++ BilletSystemCLTests/BIlTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletSystemCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletSystemCL.Tests
+{
+    [TestClass()]
+    public class BIlTests
+    {
+        [TestMethod()]
+        public void PrisTestBil()
+        {
+            var bil = new BIl();
+            int pris = bil.Pris();
+            Assert.AreEqual(240, pris);
+        }
+
+        [TestMethod()]
+        public void KøretøjTest()
+        {
+            var bil = new BIl();
+            string køretøj = bil.Køretøj();
+            Assert.AreEqual("Bil", køretøj);
+           
+        }
+    }
+}
---
+++ BilletSystemCLTests/MCTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletSystemCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletSystemCL.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void PrisTestMC()
+        {
+            var mc = new MC();
+            int pris = mc.Pris();
+            Assert.AreEqual(125, pris);
+        }
+
+        [TestMethod()]
+        public void KøretøjTest()
+        {
+            var mc = new MC();
+            string køretøj = mc.Køretøj();
+            Assert.AreEqual("Motorcykel", køretøj); 
+        }
+    }
+}
